3fff0aeb999d609183d0ac3a7d82926bf295219a
Mihir Sahay <mihir.sahay0123@gmail.com>
2020-12-12 14:05:48 +0530

    SSOS Bringup

--- ssos_phoenix.mk
+++ ssos_phoenix.mk
@@ -6,25 +6,23 @@
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common SSOS stuff.
+$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_phoenix
+PRODUCT_NAME := ssos_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
 PRODUCT_MANUFACTURER := Xiaomi
 
-# ArrowOS Properties
+# SSOS Properties
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-DEVICE_MAINTAINER := Sreekanth
+TARGET_FACE_UNLOCK_SUPPORTED := true
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.ssos.cpu=SD730
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ2A.210405.005 7181113 release-keys
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+BUILD_FINGERPRINT := POCO/phoenixin/phoenixin:11/RKQ1.200826.002/V12.1.3.0.RGHINXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
